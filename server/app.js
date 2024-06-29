5f17b81295e1032b5570fdf304e06da83d58865f
pu9052473 <pu9052473@gmail.com>
2024-06-29 18:21:07 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { dbConnection } from "./database/dbConnection.js";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRouter from "./routes/userRouter.js";
+import taskRouter from "./routes/taskRouter.js";
+
+const app = express();
+dotenv.config({ path: "./config/config.env" });
+
+// Configure CORS
+const corsOptions = {
+  origin: "http://localhost:5174", // Allow requests from this origin
+  credentials: true, // Allow sending cookies with the request
+};
+
+app.use(cors(corsOptions));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/task", taskRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+
+export default app;
